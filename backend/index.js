7b1df17b3c7e322b6836b88a84209f2f4b7b01b7
Umer Farooq <98796369+umerfarooq777@users.noreply.github.com>
2024-01-05 19:43:40 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World i am alive!')
+  res.json({message:'Hello World i am alive!'})
 })
 
 app.use('/api/', kycRoute)
